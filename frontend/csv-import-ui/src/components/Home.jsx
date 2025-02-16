2bd326f39943a5216a55cc8076fd666099ac007b
shivanshi_14 <106828642+Koniosh@users.noreply.github.com>
2025-02-16 19:50:05 +0530

    Update Home.jsx

--- frontend/csv-import-ui/src/components/Home.jsx
+++ frontend/csv-import-ui/src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div className="container">
       <h1>CSV Import System</h1>
@@ -11,6 +13,10 @@ function Home() {
         <Link to="/products">🛒 View Product Data</Link>
         <Link to="/filter-products">🔍 Filter Products by Size</Link>
       </nav>
+      <br/><br/>
+      <button onClick={() => navigate("/")} className="home-button">
+        🏠 Back to Welcome Page
+      </button>
     </div>
   );
 }
