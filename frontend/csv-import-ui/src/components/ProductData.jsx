cea767f269e38e2723b0f45a4ecfd13202705ca2
shivanshi_14 <106828642+Koniosh@users.noreply.github.com>
2025-02-16 19:50:54 +0530

    Update ProductData.jsx

--- frontend/csv-import-ui/src/components/ProductData.jsx
+++ frontend/csv-import-ui/src/components/ProductData.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function ProductData() {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then((res) => {
@@ -16,6 +18,7 @@ function ProductData() {
       <table>
         <thead>
           <tr>
+            <th>id</th>
             <th>Name</th>
             <th>Flavour</th>
             <th>Size</th>
@@ -24,6 +27,7 @@ function ProductData() {
         <tbody>
           {products.map((prod) => (
             <tr key={prod._id}>
+              <td>{prod._id}</td>
               <td>{prod.name}</td>
               <td>{prod.flavour}</td>
               <td>{prod.size}</td>
@@ -31,6 +35,9 @@ function ProductData() {
           ))}
         </tbody>
       </table>
+      <button onClick={() => navigate("/home")} className="home-button">
+        🏠 Back to Home
+      </button>
     </div>
   );
 }
